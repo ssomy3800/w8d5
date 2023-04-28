f65493505c73312bba80089c95bc009ab060f270
ssomy3800 <ssomy3800@gmail.com>
2023-04-28 10:41:17 -0700

    on  board

--- skeleton/src/piece.js
+++ skeleton/src/piece.js
@@ -1,19 +1,24 @@
 /**
  * Initializes the Piece with its color.
  */
-function Piece (color) {
+function Piece(color) {
+  this.color = color;
 }
 
 /**
  * Returns the color opposite the current piece.
  */
-Piece.prototype.oppColor = function () {
-};
+Piece.prototype.oppColor = function () {};
 
 /**
  * Changes the piece's color to the opposite color.
  */
 Piece.prototype.flip = function () {
+  if (this.color === "white") {
+    this.color = "black";
+  } else {
+    this.color = "white";
+  }
 };
 
 /**
@@ -21,10 +26,15 @@ Piece.prototype.flip = function () {
  * based on its color.
  */
 Piece.prototype.toString = function () {
+  if (this.color === "white") {
+    return "W";
+  } else {
+    return "B";
+  }
 };
 
 // DON'T TOUCH THIS CODE
-if (typeof window === 'undefined'){
-    module.exports = Piece;
+if (typeof window === "undefined") {
+  module.exports = Piece;
 }
 // DON'T TOUCH THIS CODE
